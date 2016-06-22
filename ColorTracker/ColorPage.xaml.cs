96a0f02d70cb5d2736e833b784c79f6085ad7177
garv13 <garvjasuja@outlook.com>
2016-06-23 01:38:06 +0530

    working prototype

---
+++ ColorTracker/ColorPage.xaml.cs
@@ -0,0 +1,89 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ColorTracker
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ColorPage : Page
+    {
+        public ColorPage()
+        {
+            this.InitializeComponent();
+        }
+        byte[] ar = new byte[4];
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+           
+            ar = e.Parameter as byte[];
+            colorGrid.Background = new SolidColorBrush(Color.FromArgb(ar[0],ar[1],ar[2],ar[3]));
+            CommandBar1.Visibility = Visibility.Visible;
+            InitializeComponent();
+        }
+
+        private void AppBarToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Reject Button
+            Frame.Navigate(typeof(MainPage));
+        }
+
+        private async void AppBarToggleButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            //Accept button
+            Argb ob = new Argb();
+            ob.A = ar[0];
+            ob.R = ar[1];
+            ob.G = ar[2];
+            ob.B = ar[3];
+            await fileWrite(ob);
+            Frame.Navigate(typeof(MainPage));
+        }
+
+        private async Task fileWrite(Argb p)
+        {
+            List<string> urlList = new List<string>();
+            StorageFolder localFolder = ApplicationData.Current.LocalFolder;
+            try
+            {
+                var contentsToWriteToFile = JsonConvert.SerializeObject(p);
+                urlList.Add(contentsToWriteToFile);             
+                StorageFile sampleFile = await localFolder.GetFileAsync("dataFile.txt");
+                await FileIO.AppendLinesAsync(sampleFile, urlList);
+            }
+            catch (FileNotFoundException)
+            {
+                try
+                {
+                    StorageFile sampleFile = await localFolder.CreateFileAsync("dataFile.txt",
+                    CreationCollisionOption.ReplaceExisting);
+                    await FileIO.AppendLinesAsync(sampleFile, urlList);
+                    
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+        }
+    }
+
+}
--- ColorTracker/MainPage.xaml.cs
+++ ColorTracker/MainPage.xaml.cs
@@ -5,12 +5,17 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Graphics.Imaging;
+using Windows.Media.Capture;
+using Windows.Storage;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -25,6 +30,61 @@ namespace ColorTracker
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        byte[] arr = new byte[4];
+
+        private async void get_Array(StorageFile photo)
+        {
+            IRandomAccessStream stream = await photo.OpenAsync(FileAccessMode.Read);
+            BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
+            SoftwareBitmap softwareBitmap = await decoder.GetSoftwareBitmapAsync();
+            PixelDataProvider pd = await decoder.GetPixelDataAsync();
+            byte[] bp = pd.DetachPixelData();
+            int x = (int)(decoder.PixelWidth * (decoder.PixelHeight / 2) * 4);
+            byte[] arr = new byte[4];
+            arr[1] = bp.ElementAt(x);
+            arr[2] = bp.ElementAt(x + 1);
+            arr[3] = bp.ElementAt(x + 2);
+            arr[0] = bp.ElementAt(x + 3);
+            Frame.Navigate((typeof(ColorPage)), arr);
+        }
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            CameraCaptureUI captureUI = new CameraCaptureUI();
+            captureUI.PhotoSettings.Format = CameraCaptureUIPhotoFormat.Jpeg;
+            captureUI.PhotoSettings.CroppedSizeInPixels = new Size(300, 300);
+            StorageFile photo = await captureUI.CaptureFileAsync(CameraCaptureUIMode.Photo);
+
+            if (photo == null)
+            {
+                get_Array(photo);
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private async void button1_Click(object sender, RoutedEventArgs e)
+        {
+            var picker = new Windows.Storage.Pickers.FileOpenPicker();
+            picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
+            picker.SuggestedStartLocation =
+            Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
+            picker.FileTypeFilter.Add(".jpg");
+            picker.FileTypeFilter.Add(".jpeg");
+            picker.FileTypeFilter.Add(".png");
+            Windows.Storage.StorageFile file = await picker.PickSingleFileAsync();
+
+            if (file != null)
+            {
+                get_Array(file);
+            }
+            else
+            {
+                return;
+            }
         }
     }
 }
